23c764764ab9f45eb58074ddcff3f4ad22939388
Steven Luengo <stevenluengo@gmail.com>
2015-01-26 18:36:21 -0500

    Commiting FizzBuzzer App

---
+++ majorProjects/FizzBuzz.rb
@@ -0,0 +1,68 @@
+# (1..100).each do |number|
+# 	if(number % 3 == 0) && (number % 5 == 0)
+# 		puts "FizzBuzz"
+# 	elsif number % 5 == 0
+# 		puts "Buzz"
+# 	elsif (number % 3 == 0) 
+# 		puts "Fizz"
+# 	else
+# 		puts number
+# 	end
+# end
+
+# A Program designed to FizzBuzz any range of numbers and and look for multiple of any given number
+class FizzBuzzer
+
+	def start
+
+		intro
+
+		get_numbers
+
+		calculate_fizz_buzz
+	end
+
+	def intro
+		puts "Welcome to the Fizz Buzzer App!"
+		puts "This program automates one of the most common programming questions!"
+		puts "Just plug in the numbers...let me program handle the hard part!"
+		sleep(1)
+		puts "Loading..."
+		sleep(1)
+		puts "Loading..."
+		sleep(2)
+	end
+
+	def get_numbers
+		puts "Where would you like to start the range of numbers to search through?"
+		@start_range = gets.chomp.to_i
+		puts "Where would you like to end the range of numbers to search through?"
+		@end_range = gets.chomp.to_i
+
+		puts "What number would you like to Fizz?"
+		@fizz = gets.chomp.to_i
+
+		puts "What number would you like to Buzz?"
+		@buzz = gets.chomp.to_i
+	end
+
+	def calculate_fizz_buzz
+		(@start_range..@end_range).each do |number|
+			if(number % @fizz == 0) && (number % @buzz == 0)
+				puts "FizzBuzz"
+			elsif number % @buzz == 0
+				puts "Buzz"
+			elsif (number % @fizz == 0) 
+				puts "Fizz"
+			else
+				puts number
+			end
+		end
+	end
+
+
+end
+
+
+test_one = FizzBuzzer.new
+test_one.start
